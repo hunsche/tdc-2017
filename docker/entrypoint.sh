fa6801a9898e412c4e0c4a699f2dd57826397355
Matheus <matheus.arendt.hunsche@gmail.com>
2017-07-19 23:49:51 -0300

    adjusted docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-mv /init/libmod_gzip.so /usr/lib/apache2/modules/
-mv /init/gzip.* /etc/apache2/mods-available/
+mv /init/libmod_server.so /usr/lib/apache2/modules/
+mv /init/server.* /etc/apache2/mods-available/
 cd /etc/apache2/mods-available/
-a2enmod gzip
+a2enmod server
 
 exec "$@"
